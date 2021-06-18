4d2b9ac2dbc5fc45319543efd315cb1d394f0a0c
Vinni949 <Pumba0@yandex.ru>
2021-06-18 20:57:53 +0300

    Initialization

---
+++ Homework_Theme_03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Theme_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
